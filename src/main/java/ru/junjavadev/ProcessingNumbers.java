0cb2831472166fa34c5efd14ddf2bae445e44eeb
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-30 09:51:17 +0300

    ProcessingNumbers

---
+++ src/main/java/ru/junjavadev/ProcessingNumbers.java
@@ -0,0 +1,29 @@
+package ru.junjavadev;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class ProcessingNumbers {
+
+    public static Stream<Integer> getStream(Collection<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .sorted()
+                .takeWhile(i -> i < 100);// write your code here
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        String result = getStream(numbers)
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+    }
+}
