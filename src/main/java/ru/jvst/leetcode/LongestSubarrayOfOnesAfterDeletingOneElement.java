4cd83b5d5952735165cd910bb355cd07cc3599b4
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-05-11 02:50:49 +0300

    LeetCode "1493. Longest Subarray of 1's After Deleting One Element" update javadoc

--- src/main/java/ru/jvst/leetcode/LongestSubarrayOfOnesAfterDeletingOneElement.java
+++ src/main/java/ru/jvst/leetcode/LongestSubarrayOfOnesAfterDeletingOneElement.java
@@ -1,28 +1,41 @@
 package ru.jvst.leetcode;
 
 /**
- * <a href="https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/">1493. Longest Subarray of 1's After Deleting One Element</a>
- * <p> Given a binary array nums, you should delete one element from it.
- * <p> Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.</p>
- * <blockquote><pre>
- *     Example 1:
- *     Input: nums = [1,1,0,1]
- *     Output: 3
- *     Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.</pre>
- * <pre>
- *     Example 2:
- *     Input: nums = [0,1,1,1,0,1,1,0,1]
- *     Output: 5
- *     Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].</pre>
- * <pre>
- *     Example 3:
- *     Input: nums = [1,1,1]
- *     Output: 2
- *     Explanation: You must delete one element.</pre>
- * <pre>
- *     Constraints:
- *     1 <= nums.length <= 10^5
- *     nums[i] is either 0 or 1.</pre></blockquote>
+ <a href="https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/">1493. Longest Subarray of 1's After Deleting One Element</a>
+ <div class="xFUwe" data-track-load="description_content"><p>Given a binary array <code>nums</code>, you should delete one element from it.</p>
+
+ <p>Return <em>the size of the longest non-empty subarray containing only </em><code>1</code><em>'s in the resulting array</em>. Return <code>0</code> if there is no such subarray.</p>
+
+ <p>&nbsp;</p>
+ <p><strong class="example">Example 1:</strong></p>
+
+ <pre><strong>Input:</strong> nums = [1,1,0,1]
+ <strong>Output:</strong> 3
+ <strong>Explanation:</strong> After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+ </pre>
+
+ <p><strong class="example">Example 2:</strong></p>
+
+ <pre><strong>Input:</strong> nums = [0,1,1,1,0,1,1,0,1]
+ <strong>Output:</strong> 5
+ <strong>Explanation:</strong> After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+ </pre>
+
+ <p><strong class="example">Example 3:</strong></p>
+
+ <pre><strong>Input:</strong> nums = [1,1,1]
+ <strong>Output:</strong> 2
+ <strong>Explanation:</strong> You must delete one element.
+ </pre>
+
+ <p>&nbsp;</p>
+ <p><strong>Constraints:</strong></p>
+
+ <ul>
+ <li><code>1 &lt;= nums.length &lt;= 10<sup>5</sup></code></li>
+ <li><code>nums[i]</code> is either <code>0</code> or <code>1</code>.</li>
+ </ul>
+ </div>
  **/
 
 class LongestSubarrayOfOnesAfterDeletingOneElement {
