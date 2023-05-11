25bff3b7764a0be1c5a42c83b149da5a2a36d4f9
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-05-12 02:52:03 +0300

    LeetCode "1004. Max Consecutive Ones III" update javadoc

--- src/main/java/ru/jvst/leetcode/MaxConsecutiveOnesIII.java
+++ src/main/java/ru/jvst/leetcode/MaxConsecutiveOnesIII.java
@@ -4,27 +4,34 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 /**
- * <a href="https://leetcode.com/problems/max-consecutive-ones-iii/">1004. Max Consecutive Ones III
- * </a>
- * <p> Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
- * <blockquote><pre>
- * Example 1:
- * Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
- * Output: 6
- * Explanation: [1,1,1,0,0,1,1,1,1,1,1]
- * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.</pre>
- * <pre>
- * Example 2:
- * Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
- * Output: 10
- * Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
- * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.</pre>
- * <pre>
- * Constraints:
- * 1 <= nums.length <= 105
- * nums[i] is either 0 or 1.
- * 0 <= k <= nums.length</pre></blockquote>
- * <p>
+ <a href="https://leetcode.com/problems/max-consecutive-ones-iii/">1004. Max Consecutive Ones III</a>
+ <div class="xFUwe" data-track-load="description_content"><p>Given a binary array <code>nums</code> and an integer <code>k</code>, return <em>the maximum number of consecutive </em><code>1</code><em>'s in the array if you can flip at most</em> <code>k</code> <code>0</code>'s.</p>
+
+ <p>&nbsp;</p>
+ <p><strong class="example">Example 1:</strong></p>
+
+ <pre><strong>Input:</strong> nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+ <strong>Output:</strong> 6
+ <strong>Explanation:</strong> [1,1,1,0,0,<u><strong>1</strong>,1,1,1,1,<strong>1</strong></u>]
+ Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.</pre>
+
+ <p><strong class="example">Example 2:</strong></p>
+
+ <pre><strong>Input:</strong> nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+ <strong>Output:</strong> 10
+ <strong>Explanation:</strong> [0,0,<u>1,1,<strong>1</strong>,<strong>1</strong>,1,1,1,<strong>1</strong>,1,1</u>,0,0,0,1,1,1,1]
+ Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ </pre>
+
+ <p>&nbsp;</p>
+ <p><strong>Constraints:</strong></p>
+
+ <ul>
+ <li><code>1 &lt;= nums.length &lt;= 10<sup>5</sup></code></li>
+ <li><code>nums[i]</code> is either <code>0</code> or <code>1</code>.</li>
+ <li><code>0 &lt;= k &lt;= nums.length</code></li>
+ </ul>
+ </div>
  **/
 
 /*
