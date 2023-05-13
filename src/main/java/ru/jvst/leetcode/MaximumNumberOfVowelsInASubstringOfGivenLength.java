ef09278e52eb3481e0084d64aaa1b0acb183e9a5
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-05-14 02:54:45 +0300

    LeetCode "1456. Maximum Number of Vowels in a Substring of Given Length" update javadoc

--- src/main/java/ru/jvst/leetcode/MaximumNumberOfVowelsInASubstringOfGivenLength.java
+++ src/main/java/ru/jvst/leetcode/MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -1,27 +1,42 @@
 package ru.jvst.leetcode;
 
 /**
- * <a href="https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/">1456. Maximum Number of Vowels in a Substring of Given Length</a>
- * <p> Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
- * <p> Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
- * <blockquote><pre>
- * Example 1:
- * Input: s = "abciiidef", k = 3
- * Output: 3
- * Explanation: The substring "iii" contains 3 vowel letters.
- * Example 2:
- * Input: s = "aeiou", k = 2
- * Output: 2
- * Explanation: Any substring of length 2 contains 2 vowels.
- * Example 3:
- * Input: s = "leetcode", k = 3
- * Output: 2
- * Explanation: "lee", "eet" and "ode" contain 2 vowels.
- * <pre>
- * Constraints:
- * 1 <= s.length <= 105
- * s consists of lowercase English letters.zz
- * 1 <= k <= s.length</pre></blockquote>
+ <a href="https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/">1456. Maximum Number of Vowels in a Substring of Given Length</a>
+ <div class="xFUwe" data-track-load="description_content"><p>Given a string <code>s</code> and an integer <code>k</code>, return <em>the maximum number of vowel letters in any substring of </em><code>s</code><em> with length </em><code>k</code>.</p>
+
+ <p><strong>Vowel letters</strong> in English are <code>'a'</code>, <code>'e'</code>, <code>'i'</code>, <code>'o'</code>, and <code>'u'</code>.</p>
+
+ <p>&nbsp;</p>
+ <p><strong class="example">Example 1:</strong></p>
+
+ <pre><strong>Input:</strong> s = "abciiidef", k = 3
+ <strong>Output:</strong> 3
+ <strong>Explanation:</strong> The substring "iii" contains 3 vowel letters.
+ </pre>
+
+ <p><strong class="example">Example 2:</strong></p>
+
+ <pre><strong>Input:</strong> s = "aeiou", k = 2
+ <strong>Output:</strong> 2
+ <strong>Explanation:</strong> Any substring of length 2 contains 2 vowels.
+ </pre>
+
+ <p><strong class="example">Example 3:</strong></p>
+
+ <pre><strong>Input:</strong> s = "leetcode", k = 3
+ <strong>Output:</strong> 2
+ <strong>Explanation:</strong> "lee", "eet" and "ode" contain 2 vowels.
+ </pre>
+
+ <p>&nbsp;</p>
+ <p><strong>Constraints:</strong></p>
+
+ <ul>
+ <li><code>1 &lt;= s.length &lt;= 10<sup>5</sup></code></li>
+ <li><code>s</code> consists of lowercase English letters.</li>
+ <li><code>1 &lt;= k &lt;= s.length</code></li>
+ </ul>
+ </div>
  **/
 
 class MaximumNumberOfVowelsInASubstringOfGivenLength {
