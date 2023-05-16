111ba193251af781a6359cfcf708dfe60fcc2894
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-05-17 02:58:14 +0300

    LeetCode "283. Move Zeroes" update javadoc

--- src/main/java/ru/jvst/leetcode/MoveZeroes.java
+++ src/main/java/ru/jvst/leetcode/MoveZeroes.java
@@ -3,22 +3,29 @@ package ru.jvst.leetcode;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.com/problems/move-zeroes/">283. Move Zeroes</a>
- * <p> Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
- * <p> Note that you must do this in-place without making a copy of the array.
- * <blockquote><pre>
- * Example 1:
- * Input: nums = [0,1,0,3,12]
- * Output: [1,3,12,0,0]
- * Example 2:
- *
- * Input: nums = [0]
- * Output: [0]</pre>
- * <pre>
- * Constraints:
- * 1 <= nums.length <= 104
- * -231 <= nums[i] <= 231 - 1</pre></blockquote>
- * <p> Follow up: Could you minimize the total number of operations done?
+ <a href="https://leetcode.com/problems/move-zeroes/">283. Move Zeroes</a>
+ <div class="xFUwe" data-track-load="description_content"><p>Given an integer array <code>nums</code>, move all <code>0</code>'s to the end of it while maintaining the relative order of the non-zero elements.</p>
+
+ <p><strong>Note</strong> that you must do this in-place without making a copy of the array.</p>
+
+ <p>&nbsp;</p>
+ <p><strong class="example">Example 1:</strong></p>
+ <pre><strong>Input:</strong> nums = [0,1,0,3,12]
+ <strong>Output:</strong> [1,3,12,0,0]
+ </pre><p><strong class="example">Example 2:</strong></p>
+ <pre><strong>Input:</strong> nums = [0]
+ <strong>Output:</strong> [0]
+ </pre>
+ <p>&nbsp;</p>
+ <p><strong>Constraints:</strong></p>
+
+ <ul>
+ <li><code>1 &lt;= nums.length &lt;= 10<sup>4</sup></code></li>
+ <li><code>-2<sup>31</sup> &lt;= nums[i] &lt;= 2<sup>31</sup> - 1</code></li>
+ </ul>
+
+ <p>&nbsp;</p>
+ <strong>Follow up:</strong> Could you minimize the total number of operations done?</div>
  **/
 
 class MoveZeroes {
