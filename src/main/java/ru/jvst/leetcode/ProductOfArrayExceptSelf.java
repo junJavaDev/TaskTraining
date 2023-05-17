46958b694375a1efa554c9970315fd71ceeb090f
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-05-18 02:59:19 +0300

    LeetCode "238. Product of Array Except Self" update javadoc

--- src/main/java/ru/jvst/leetcode/ProductOfArrayExceptSelf.java
+++ src/main/java/ru/jvst/leetcode/ProductOfArrayExceptSelf.java
@@ -2,24 +2,33 @@ package ru.jvst.leetcode;
 import java.util.Arrays;
 
 /**
-<a href="https://leetcode.com/problems/product-of-array-except-self/">238. Product of Array Except Self</a>
- <p> Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
-The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
- <p> You must write an algorithm that runs in O(n) time and without using the division operation.
-<blockquote><pre>
- Example 1:
- Input: nums = [1,2,3,4]
- Output: [24,12,8,6]</pre>
- <pre>
- Example 2:
- Input: nums = [-1,1,0,-3,3]
- Output: [0,0,9,0,0]</pre>
- <pre>
- Constraints:
- 2 <= nums.length <= 105
- -30 <= nums[i] <= 30</pre></blockquote>
- <p> The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+ <a href="https://leetcode.com/problems/product-of-array-except-self/">238. Product of Array Except Self</a>
+ <div class="xFUwe" data-track-load="description_content"><p>Given an integer array <code>nums</code>, return <em>an array</em> <code>answer</code> <em>such that</em> <code>answer[i]</code> <em>is equal to the product of all the elements of</em> <code>nums</code> <em>except</em> <code>nums[i]</code>.</p>
+
+ <p>The product of any prefix or suffix of <code>nums</code> is <strong>guaranteed</strong> to fit in a <strong>32-bit</strong> integer.</p>
+
+ <p>You must write an algorithm that runs in&nbsp;<code>O(n)</code>&nbsp;time and without using the division operation.</p>
+
+ <p>&nbsp;</p>
+ <p><strong class="example">Example 1:</strong></p>
+ <pre><strong>Input:</strong> nums = [1,2,3,4]
+ <strong>Output:</strong> [24,12,8,6]
+ </pre><p><strong class="example">Example 2:</strong></p>
+ <pre><strong>Input:</strong> nums = [-1,1,0,-3,3]
+ <strong>Output:</strong> [0,0,9,0,0]
+ </pre>
+ <p>&nbsp;</p>
+ <p><strong>Constraints:</strong></p>
+
+ <ul>
+ <li><code>2 &lt;= nums.length &lt;= 10<sup>5</sup></code></li>
+ <li><code>-30 &lt;= nums[i] &lt;= 30</code></li>
+ <li>The product of any prefix or suffix of <code>nums</code> is <strong>guaranteed</strong> to fit in a <strong>32-bit</strong> integer.</li>
+ </ul>
+
+ <p>&nbsp;</p>
+ <p><strong>Follow up:</strong>&nbsp;Can you solve the problem in <code>O(1)</code>&nbsp;extra&nbsp;space complexity? (The output array <strong>does not</strong> count as extra space for space complexity analysis.)</p>
+ </div>
  **/
 
 ///********** Short ***************/
